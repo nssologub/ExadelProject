3a8a82e2eaeffd3f9e14a810506c4230010893c6
nssologub <nssologub@gmail.com>
2014-12-16 11:38:47 +0300

    first

--- src/SQL/createTables.sql
+++ src/SQL/createTables.sql
@@ -2,6 +2,6 @@ CREATE DATABASE 'ExadelProject';
 USE 'ExadelProject';
 DROP TABLE IF EXISTS 'Person';
 CREATE TABLE 'Person'(
-id int(11) NOT NULL AUTO_INCREMENT
-
+id int(11) NOT NULL AUTO_INCREMENT,
+firstname varchar(30)
 )
